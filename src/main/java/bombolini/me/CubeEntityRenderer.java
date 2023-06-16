346f5f47b2bc067cbee74c79ee1fc48544f358a2
Kaiwen Tsou <kktsou@ucsd.edu>
2023-06-15 22:22:55 -0700

    Tutorial

---
+++ src/main/java/bombolini/me/CubeEntityRenderer.java
@@ -0,0 +1,20 @@
+package bombolini.me;
+
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.util.Identifier;
+
+/*
+ * A renderer is used to provide an entity model, shadow size, and texture.
+ */
+public class CubeEntityRenderer extends MobEntityRenderer<CubeEntity, CubeEntityModel> {
+
+    public CubeEntityRenderer(EntityRendererFactory.Context context) {
+        super(context, new CubeEntityModel(context.getPart(EntityTestingClient.MODEL_CUBE_LAYER)), 0.5f);
+    }
+
+    @Override
+    public Identifier getTexture(CubeEntity entity) {
+        return new Identifier("template-mod", "textures/entity/cube/cube.png");
+    }
+}
--- src/main/java/bombolini/me/ExampleMod.java
+++ src/main/java/bombolini/me/ExampleMod.java
@@ -3,6 +3,11 @@ package bombolini.me;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -14,7 +19,20 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("template-mod");
-	public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
+	public static final Item CUSTOM_ITEM = new CustomItem(new FabricItemSettings());
+
+	/*
+	 * Registers our Cube Entity under the ID "ExampleMod:cube".
+	 *
+	 * The entity is registered under the SpawnGroup#CREATURE category, which is what most animals and passive/neutral mobs use.
+	 * It has a hitbox size of .75x.75, or 12 "pixels" wide (3/4ths of a block).
+	 */
+	public static final EntityType<CubeEntity> CUBE = Registry.register(
+			Registry.ENTITY_TYPE,
+			new Identifier("template-mod", "cube"),
+			FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, CubeEntity::new).dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build()
+	);
+
 
 	@Override
 	public void onInitialize() {
@@ -23,9 +41,18 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello world!");
-		LOGGER.info(" " + (CUSTOM_ITEM != null));
 
-		Registry.register(Registry.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
+		Registry.register(Registry.ITEM, new Identifier("template-mod", "custom_item"), CUSTOM_ITEM);
+
+		/*
+		 * Register our Cube Entity's default attributes.
+		 * Attributes are properties or stats of the mobs, including things like attack damage and health.
+		 * The game will crash if the entity doesn't have the proper attributes registered in time.
+		 *
+		 * In 1.15, this was done by a method override inside the entity class.
+		 * Most vanilla entities have a static method (eg. ZombieEntity#createZombieAttributes) for initializing their attributes.
+		 */
+		FabricDefaultAttributeRegistry.register(CUBE, CubeEntity.createMobAttributes());
 
 	}
 }
